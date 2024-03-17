e896653793a0455a745fdfb168d814e2ef2fdea6
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-03-17 16:53:11 +0530

    Finishing touch

--- script.js
+++ script.js
@@ -1,17 +1,86 @@
-let col = ["red","green","blue","purple"];
-let h3 = document.querySelector('h3');
+let gameSeq = [];
+let userSeq =[];
+
+let btns = ["red","green","blue","purple"];
+
 let statement = false;
+let level =0;
+
+let h3 = document.querySelector('h3');
+
+window.addEventListener("keypress",function(){
+    if(statement == false){
+    statement = true;
+    levelUp();
+    }
+});
+
 window.addEventListener("click",function(){
     if(statement == false){
-    h3.innerHTML = "Game Started";
     statement = true;
+    levelUp();
     }
-})
+});
 
-function blink(color){
-    let div = document.querySelector(`.${color}`)
+function gameFlash(btn){
+    btn.classList.add("blink");
     
-    setTimeout(()=>{
+    setTimeout(function () {
+        btn.classList.remove("blink");
+    }, 200);
+}
 
-    })
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+function levelUp(){
+    userSeq = [];
+    level++;
+    h3.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    if(userSeq[idx] === gameSeq[idx]){
+        if (userSeq.length == gameSeq.length){
+            setTimeout(levelUp , 1000);
+        }
+    }
+    else{
+        h3.innerHTML =  `Game Over! Your Score is ${level} Press Any key to Start.`;
+        reset();
+    }
+}
+
+function btnPress() {
+    let btn =this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+
+    checkAns(userSeq.length -1);
+}
+
+let allBtns = document.querySelectorAll(".box");
+for (btn of allBtns){
+    btn.addEventListener("click" , btnPress);
+}
+
+function reset(){
+    statement = false;
+    gameSeq = [];
+    userSeq =[];
+    level = 0;
 }
